4dea83faab07de2c4208a4af1218337643403816
Paulim18 <143541887+Paulim18@users.noreply.github.com>
2025-05-30 19:44:04 -0300

    Add files via upload

---
+++ trabalho_anderson/Teste.java
@@ -0,0 +1,15 @@
+/**
+ * Classe de teste que imprime uma mensagem.
+ */
+public class Teste {
+  /**
+   * Método principal.
+   *
+   * @param args argumentos de linha de comando (não utilizados).
+   */
+  public static void main(String[] args) {
+    if (true) {
+      System.out.println("Tudo certo, sem erros!");
+    }
+  }
+}
